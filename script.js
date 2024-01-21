38dc202df26132d44863f0139a7674054263eda0
CarmanschiDenis <144582287+CarmanschiDenis@users.noreply.github.com>
2024-01-21 10:57:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// function checkNum(){
+//     const inpt = document.getElementById("input");
+//     if(!inpt.checkValidity()){
+//         document.getElementById("success").innerHTML = inpt.validationMessage
+//     }else{
+//         document.getElementById("success").innerHTML = "input ok"
+//     }
+// }
+
+
+
+
+
+// const msg = document.getElementById("success");
+// function getLocation(){
+//     if (navigator.geolocation){
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     }else{
+//         msg.innerHTML = "ERROR";
+//     }
+    
+
+
+
+// }
+// function showPosition(position){
+//    let pos = "Latitude: " + position.coords.latitude + "<br>Longitude: "+ position.coords.longitude;
+//     localStorage.setItem("poss", pos);
+// }
+
+
+// localStorage.setItem("name","max");
+// document.getElementById("str").innerHTML = localStorage.getItem("poss");
+
+//localStorage --- sessionStorage
+//ramane let   ---  dispare let dupa ce inchizi browser-ul
+
+
+
+
+
+//request -> server
+// xmlHTTP
+
+
+// async function fetchText(){
+//     let response = await fetch('readme.txt');
+//     let data = response.text();
+//     console.log(data);
+// }
+
